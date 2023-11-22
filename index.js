293b7327f68074a2f143f31fdfcba2e3d686c4ae
creatingweb123 <71700107+creatingweb123@users.noreply.github.com>
2023-11-22 22:56:23 +0900

    Visited Country Project

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 import express from "express";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "1234",
+  port: 5432,
+});
+db.connect();
 
-app.get("/",(req, res)=>{
-    res.render("index.ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisited(){
+  const result = await db.query("SELECT * FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country)=>{
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
+  res.render('index.ejs',{
+    total: countries.length,
+    countries: countries,
+  });
 });
 
+app.post("/add", async (req, res) => {
+  const new_country = req.body.country;
+  try{
+    const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%'||$1||'%'",
+    [new_country.toLowerCase()]
+    );
+
+    const country_code = result.rows[0].country_code;
+    console.log(country_code);
+    try{
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",
+      [country_code]
+      );
+      res.redirect('/');
+    } catch(err){
+      console.log(err);
+      const countries = await checkVisited();
+      res.render('index.ejs',{
+        total: countries.length,
+        countries: countries,
+        error : "Country has already been added, try again",
+      });
+    }
+  } catch(err){
+    console.log(err);
+    const countries = await checkVisited();
+    res.render('index.ejs',{
+      total: countries.length,
+      countries: countries,
+      error : "Country name does not exist, try again",
+    });
+  }
+});
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
